96cd0a16663fae65d3852f9558e395cf1ccbafdc
RonnieAshlock <roashloc@microsoft.com>
2020-07-31 09:53:25 -0700

    Modifying test asset manifest

--- Apps/Playground/SampleScene.ts
+++ Apps/Playground/SampleScene.ts
@@ -44,9 +44,51 @@ export class SampleScene {
     //this.model = BABYLON.Mesh.CreateBox("box", 0.3, this.scene);
     //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoxTextured/glTF/BoxTextured.gltf");
     //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://poly.googleusercontent.com/downloads/c/fp/1595152286652373/6xn1irne__S/aduhRbNxsqH/Sonic%20the%20Hedgehog.gltf");
-    const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://poly.googleusercontent.com/downloads/c/fp/1595324547122487/4YajIweD1pN/fAfFoekkdQl/model.gltf");
-    console.error("loaded model.");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://poly.googleusercontent.com/downloads/c/fp/1595324547122487/4YajIweD1pN/fAfFoekkdQl/model.gltf");
     //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/CesiumMan/glTF/CesiumMan.gltf");
+    
+    //Models with Progressively larger textures
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Head/Head_NoTex.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Head/Head_512Tex.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Head/Head_1KTex.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Head/Head_2KTex.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Head/Head_4KTex.glb");
+
+    //3D Scans
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/DwarfGoat.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/EnglishBulldog.glb");;
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/FireHydrant.glb");
+    
+    //Static Models of varying complexity and size
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/CoffeeMug.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/HospitalBed.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/WarehouseShelf.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/WarehouseShelfPieces.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/IndustrialEspresso.glb");
+    const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/CherryPicker.glb");
+    
+    //Animated Models
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Bendy.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Alien.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/ZeroGravity.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/BabylonJS/Assets/master/meshes/alien.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/SolarSystem.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Sun.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Mercury.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Venus.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Earth.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Mars.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Jupiter.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Saturn.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Neptune.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/Uranus.glb");
+
+
+    // Lighting scenarios
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/GLB/SphereLights.glb");
+    //const newModel = await BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/RonnieAshlock/TestAssets/master/BabylonScenes/SphereLights.babylon");
+    
+    console.error("loaded model.");
     this.model = newModel.meshes[0];
 
     // Position the model in front of the camera.
@@ -55,7 +97,7 @@ export class SampleScene {
     // Set the target scale to cap the size of the model to targetScale meters tall.
     this.targetScale = this.targetScale / (max.y - min.y);
 
-    this.model.position = this.camera.position.add(this.camera.getForwardRay().direction.scale(this.targetScale * 5));
+    this.model.position = this.camera.position.add(this.camera.getForwardRay().direction.scale(this.targetScale * 10));
     this.model.scalingDeterminant = 0;
     this.model.lookAt(this.camera.position);
     this.camera.setTarget(this.model);
